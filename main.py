c3d17223e68e3defd09476cb493613b6143133d3
Marin Roussel <marinroussel@Marins-MacBook-Pro.local>
2026-01-30 13:25:43 +0100

    debut du prototype

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def testcommit(message):
-    print(message)
+import sys                       
+from PySide6.QtCore import Qt   
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+from PySide6.QtGui import QIcon, QPixmap  
 
-testcommit("I m a witch")
+
+
